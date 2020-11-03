e7d41b6a0f99f96919f9c49fa43d3a310162f43c
Evren Alim <evrenalim@gmail.com>
2020-11-03 22:29:56 +0100

    Created ANN-BackwardV2 for corrections to work with 6 samples

--- A1-Backpropagation/ACML_A1_Backpropagation.py
+++ A1-Backpropagation/ACML_A1_Backpropagation.py
@@ -44,7 +44,7 @@ XS = np.array([
 ### End of Array Initialization
 
 # 8 training sample, use multiple time same samples
-#first layer out 3x1 = 3x9 x 9x1
+#first layer out 3x6 = 3x8 x 8x6
 #second layer out 8x1 = 8x4 (after adding bias) x 4x1
 N_HIDDEN_NODES = 3
 N_INPUT_FEATURES = 8 # Add 1 for BIAS X0
@@ -59,47 +59,62 @@ B = np.array([
     [1]
 ])
 
-ITER=8000000
+B1A = np.array([
+    [1, 1, 1, 1, 1, 1, 1, 1]
+])
+
+B2A = np.array([
+    [1, 1, 1, 1, 1, 1, 1, 1]
+])
+
+weightDecay = 0.001
+learningRate = 0.1
+
+ITER=400000
 for currentIter in range(1, ITER):
     i = currentIter % 9
     if i != 0:
         #print(i)
-        X1 = X[:, i-1:i]
+        #X1 = X[:, i-1:i]
         # adding bias
-        X1 = np.concatenate((B, X1), axis=0)
-        y1 = X[:, i-1:i]
+        #X1 = np.concatenate((B, X1), axis=0)
+        X1 = np.concatenate((B1A, X), axis=0)
+        #y1 = X[:, i-1:i]
         Z2 = np.dot(W1, X1)
         L1Out = sigmoid(Z2)
         #print("Out First Layer :" + str(L1Out))
 
         # For second layer
-        L1Out = np.concatenate((B, L1Out), axis=0)
+        L1Out = np.concatenate((B2A, L1Out), axis=0)
         Z3 = np.dot(W2, L1Out)
         L2Out = sigmoid(Z3)
         #print("Out Second Layer :" + str(L2Out))
 
-        L2Error = y1 - L2Out
+        L2Error = L2Out - X #y1 #y1 - L2Out
         L2Delta = L2Error * dersigmoid(L2Out)
+        #L2Delta = L2Delta[1:, :]
         L1Error =  np.dot(W2.T, L2Delta)
         L1Delta = L1Error * dersigmoid(L1Out)
-        W2 += 0.01 * np.dot(L2Delta, L1Out.T)
+        W2 -= learningRate*(np.dot(L2Delta, L1Out.T) + weightDecay * W2) 
         L1Delta = L1Delta[1:, :]
-        W1 += 0.01 * np.dot(L1Delta, X1.T)
+        W1 -= learningRate*(np.dot(L1Delta, X1.T) + weightDecay * W1)
 
 print("After training prediction performance :")
-print(y1 - L2Out)
-print(y1)
+#print(y1 - L2Out)
+print(X)
 print(L2Out)
+print(sigmoid(np.dot(W1, X1)))
+
 
 # Use trained weights for testing
-X1 = X[:, 1:2]
-X1 = np.concatenate((B, X1), axis=0)
-y1 = X[:, 1:2]
-Z2 = np.dot(W1, X1)
-L1Out = sigmoid(Z2)
-L1Out = np.concatenate((B, L1Out), axis=0)
-Z3 = np.dot(W2, L1Out)
-L2Out = sigmoid(Z3)
+#X1 = X[:, 1:2]
+#X1 = np.concatenate((B, X1), axis=0)
+#y1 = X[:, 1:2]
+#Z2 = np.dot(W1, X1)
+#L1Out = sigmoid(Z2)
+#L1Out = np.concatenate((B, L1Out), axis=0)
+#Z3 = np.dot(W2, L1Out)
+#L2Out = sigmoid(Z3)
 
-print(y1)
-print(L2Out)
+#print(y1)
+#print(L2Out)
