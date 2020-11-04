2d7a89ff99364eb7068d8e9cd2ac6b1b4fad7d38
Evren Alim <evrenalim@gmail.com>
2020-11-04 07:44:45 +0100

    Final Update

--- ANN-backward.py
+++ ANN-backward.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 sns.set(style='darkgrid')
 
 # define the sigmoid function and derivatives sigmoid
@@ -36,41 +37,49 @@ hidden_dim = 3
 output_dim = 8
 
 # Initialize weight decay parameter lambda
-weightDecay = 0.001
+weightDecay = 0  #0.0001, 0.0001, 0.001
 learningCurve =[]
+m = len(X)
 
-# Start training
-for learningRate in [0.01, 0.05, 0.1]:
-    
+start = time.time()
+
+# Start training 
+for learningRate in [0.1, 0.5, 1]:
     # Initialize random weights 
-    W1 = np.random.normal(0,0.01,size=(input_dim, hidden_dim))
-    W2 = np.random.normal(0,0.01,size=(hidden_dim, output_dim))
+    W1 = np.random.randn(hidden_dim, input_dim)
+    W2 = np.random.randn(output_dim, hidden_dim)
     # Initialize random bias term
-    b1 = np.random.normal(0,0.01, size=(input_dim, hidden_dim))
-    b2 = np.random.normal(0,0.01, size=(input_dim, output_dim))
+    b1 = np.zeros((hidden_dim, 1))
+    b2 = np.zeros((output_dim, 1))
     
-    for i in range(1,50000):
+    for i in range(1,500000):
         # forward
         l0 = X
-        l1 = sigmoid(np.dot(l0, W1)+b1)
-        l2 = sigmoid(np.dot(l1, W2)+b2)
+        l1 = sigmoid(np.dot(W1, l0) +b1)
+        l2 = sigmoid(np.dot(W2, l1) +b2)
         # backward
-        l2_error = l2 - y
-        l2_delta = l2_error*dersigmoid(l2)
-        l1_error = l2_delta.dot(W2.T)
-        l1_delta = l1_error*dersigmoid(l1)
+        l2_delta = (l2 - y)*dersigmoid(l2)
+        l1_delta = np.dot(W2.T,l2_delta)*dersigmoid(l1)
         # Updata weights and bias
-        W2 -= learningRate*(l1.T.dot(l2_delta) + weightDecay * W2)
-        W1 -= learningRate*(l0.T.dot(l1_delta) + weightDecay * W1)
-        b1 -= learningRate*l1_delta
-        b2 -= learningRate*l2_delta
+        gradientW2 = np.dot(l2_delta, l1.T)
+        gradientW1 = np.dot(l1_delta, l0.T)
+        gradientb2 = np.sum(l2_delta,axis=1,keepdims=True)
+        gradientb1 = np.sum(l1_delta,axis=1,keepdims=True)
+        W2 -= learningRate*(1/m * gradientW2 + weightDecay * W2)
+        W1 -= learningRate*(1/m * gradientW1 + weightDecay * W1)
+        b1 -= learningRate* 1/m * gradientb1
+        b2 -= learningRate* 1/m * gradientb2
         # store the error for learning curve
-        if (i%1000==0):
-            learningCurve.append([i, learningRate, np.sum(1/2 * np.dot(l2_error.T, l2_error))])
+        if (i%10000==0):
+            learningCurve.append([i, learningRate, np.sum(1/2 * np.dot((l2 - y).T, l2 - y))])
             
-
+print("Execution duration: " + str((time.time() - start)))
+print("Output: ")
 print(l2)
+print("y: ")
 print(y)
+print("Output of hidden layer: ")
+print(l1)
 learningCurveDf = pd.DataFrame(learningCurve, columns=('currentIter','learningRate', 'MSE'))
 sns.lineplot(x='currentIter', y='MSE', hue= 'learningRate',legend='full',data=learningCurveDf)
 plt.show()
--- README.md
+++
@@ -1,2 +0,0 @@
-# ACML
-ACML-Projects
